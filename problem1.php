0c6ea34338bb18dcb651c79f17ab1fd59b4962f0
Amit Merchant <bullredeyes@gmail.com>
2015-01-02 19:19:24 +0530

    Solved first problem

--- problem1.php
+++ problem1.php
@@ -5,6 +5,14 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 */
 
+$sum = 0;
 
+for($i=0; $i<1000; $i++){
+  if($i%3==0 || $i%5==0){
+    $sum = $sum+$i;
+  }
+}
+
+echo "Sum of all the multiples of 3 or 5 below 1000 :".$sum;
 
 ?>
